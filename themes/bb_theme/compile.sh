8f4f34f49a0d7d274b19f4b4c678eeef1b02ae9e
Benjamin Chodoroff <ben@theworkdept.com>
2014-05-04 15:32:54 -0700

    oops copy+paste :)

--- themes/bb_theme/compile.sh
+++ themes/bb_theme/compile.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-lessc --compress --source-map=css/style.map --source-map-rootpath=/profiles/onboard_profile/themes/onboard_theme/ --source-map-basepath=`pwd` less/style.less > css/style.css
+lessc --compress --source-map=css/style.map --source-map-rootpath=/profiles/commerce_kickstart/themes/bb_theme/ --source-map-basepath=`pwd` less/style.less > css/style.css
