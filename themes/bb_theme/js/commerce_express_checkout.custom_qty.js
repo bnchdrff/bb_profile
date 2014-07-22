64a36669d9a7e8dfa4a1b2d23db9571c5fbc8fb2
Benjamin Chodoroff <ben@theworkdept.com>
2014-07-22 16:10:31 -0400

    basic qty widget done

--- themes/bb_theme/js/commerce_express_checkout.custom_qty.js
+++ themes/bb_theme/js/commerce_express_checkout.custom_qty.js
@@ -2,11 +2,32 @@
 
 Drupal.behaviors.cec_custom_qty = {
   attach: function(context, settings) {
-    $('.qty_ctl', context).on('keydown keyup click input submit mouseenter', function(ev) {
+    var $qty_ctl = $('.qty_ctl', context);
+    var cb_context = {
+      qty_ctl: $qty_ctl[0],
+      max_qty: $('.field-name-commerce-product-limit-max .field-item').text() * 1
+    };
+    $qty_ctl.on('keydown keyup click input submit mouseenter', function(ev) {
       var qty = this.value;
-      var $a = $(this).siblings('a').eq(0);
+      var $a = $(this).parent().parent().find('a.commerce_express_checkout').eq(0);
       var $a_href = $a.attr('href');
       $a.attr('href', $a_href.replace(/(.*commerce-express-checkout\/\d*)\/\d*/, '$1/' + qty));
+    });
+    $('a.increase_qty', context).on('click', null, cb_context, function(ev) {
+      ev.preventDefault();
+      ev.data.qty_ctl.value++;
+      if (ev.data.qty_ctl.value > ev.data.max_qty) {
+        ev.data.qty_ctl.value = ev.data.max_qty;
+      }
+      $(this).siblings('input.qty_ctl').click();
+    });
+    $('a.decrease_qty', context).on('click', null, cb_context, function(ev) {
+      ev.preventDefault();
+      ev.data.qty_ctl.value--;
+      if (ev.data.qty_ctl.value < 1) {
+        ev.data.qty_ctl.value = 1;
+      }
+      $(this).siblings('input.qty_ctl').click();
     });
   }
 };
--- themes/bb_theme/theme/commerce_express_checkout.tpl.php
+++ themes/bb_theme/theme/commerce_express_checkout.tpl.php
@@ -10,7 +10,11 @@
  * - $custom_qty: Whether or not to allow custom quantity.
  */
 ?>
-<?php print $link; ?>
 <?php if ($custom_qty): ?>
-<input class="qty_ctl" id="qty_ctl_<?php print $product_id; ?>" type="text" size="2" value="" type="number" min="1" max="999" placeholder="Qty" />
+<div class="qtl_ctl_widget">
+  <a class="increase_qty" href="#">&uarr;</a>
+  <input class="qty_ctl" id="qty_ctl_<?php print $product_id; ?>" type="text" size="2" value="1" type="number" min="1" max="999" placeholder="Qty" />
+  <a class="decrease_qty" href="#">&darr;</a>
+</div>
 <?php endif; ?>
+<a class="commerce_express_checkout" href="<?php print $path; ?>">ORDER</a>
