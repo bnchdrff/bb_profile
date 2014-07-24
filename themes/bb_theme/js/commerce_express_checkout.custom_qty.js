d513c080003284ce94146749c0cb59830348e585
Benjamin Chodoroff <ben@theworkdept.com>
2014-07-24 14:58:31 -0400

    add-to-cart widget

--- themes/bb_theme/js/commerce_express_checkout.custom_qty.js
+++ themes/bb_theme/js/commerce_express_checkout.custom_qty.js
@@ -3,30 +3,52 @@
 Drupal.behaviors.cec_custom_qty = {
   attach: function(context, settings) {
     var $qty_ctl = $('.qty_ctl', context);
+    var $qty_delivered = $('.qty_delivered', context);
+    var $qty_error = $('.qty_error', context);
+    // this shouldn't live here!!! but there isn't an easy endpoint to ask afaik
+    var price_check = function price_check(num_cans) {
+      if (num_cans < 2) {
+        return num_cans * 19;
+      } else {
+        return num_cans * 17;
+      }
+    };
+
     var cb_context = {
       qty_ctl: $qty_ctl[0],
+      qty_delivered: $qty_delivered[0],
+      qty_error: $qty_error[0],
       max_qty: $('.field-name-commerce-product-limit-max .field-item').text() * 1
     };
-    $qty_ctl.on('keydown keyup click input submit mouseenter', function(ev) {
-      var qty = this.value;
-      var $a = $(this).parent().parent().find('a.commerce_express_checkout').eq(0);
-      var $a_href = $a.attr('href');
-      $a.attr('href', $a_href.replace(/(.*commerce-express-checkout\/\d*)\/\d*/, '$1/' + qty));
-    });
-    $('a.increase_qty', context).on('click', null, cb_context, function(ev) {
-      ev.preventDefault();
-      ev.data.qty_ctl.value++;
+
+    $qty_ctl.on('keydown keyup click input submit mouseenter', null, cb_context, function(ev) {
       if (ev.data.qty_ctl.value > ev.data.max_qty) {
         ev.data.qty_ctl.value = ev.data.max_qty;
       }
-      $(this).siblings('input.qty_ctl').click();
-    });
-    $('a.decrease_qty', context).on('click', null, cb_context, function(ev) {
-      ev.preventDefault();
-      ev.data.qty_ctl.value--;
       if (ev.data.qty_ctl.value < 1) {
         ev.data.qty_ctl.value = 1;
       }
+      if (ev.data.qty_ctl.value == 5) {
+        ev.data.qty_error.innerHTML = 'If you want more than 5 cans, <a href="mailto:info@beardbalm.us">email us!</a>';
+      }
+      if (ev.data.qty_ctl.value < 5) {
+        ev.data.qty_error.innerHTML = '';
+      }
+      var $a = $(this).parent().parent().find('a.commerce_express_checkout').eq(0);
+      var $a_href = $a.attr('href');
+      $a.attr('href', $a_href.replace(/(.*commerce-express-checkout\/\d*)\/\d*/, '$1/' + ev.data.qty_ctl.value));
+      ev.data.qty_delivered.innerHTML = price_check(ev.data.qty_ctl.value*1);
+    });
+
+    $('a.increase_qty', context).on('click', null, cb_context, function(ev) {
+      ev.preventDefault();
+      ev.data.qty_ctl.value++;
+      $(this).siblings('input.qty_ctl').click();
+    });
+
+    $('a.decrease_qty', context).on('click', null, cb_context, function(ev) {
+      ev.preventDefault();
+      ev.data.qty_ctl.value--;
       $(this).siblings('input.qty_ctl').click();
     });
   }
--- themes/bb_theme/theme/commerce_express_checkout.tpl.php
+++ themes/bb_theme/theme/commerce_express_checkout.tpl.php
@@ -10,11 +10,14 @@
  * - $custom_qty: Whether or not to allow custom quantity.
  */
 ?>
+
 <?php if ($custom_qty): ?>
-<div class="qtl_ctl_widget">
-  <a class="increase_qty" href="#">&uarr;</a>
+<span class="qtl_ctl_widget">
+  <a class="increase_qty glyphicon glyphicon-arrow-up" href="#"></a>
   <input class="qty_ctl" id="qty_ctl_<?php print $product_id; ?>" type="text" size="2" value="1" type="number" min="1" max="999" placeholder="Qty" />
-  <a class="decrease_qty" href="#">&darr;</a>
-</div>
+  <a class="decrease_qty glyphicon glyphicon-arrow-down" href="#"></a>
+</span>
 <?php endif; ?>
-<a class="commerce_express_checkout" href="<?php print $path; ?>">ORDER</a>
+<a type="button" class="btn btn-default btn-md commerce_express_checkout" href="<?php print $path; ?>">ORDER</a>
+<span class="qty_delivered_wrap">Delivered for just $<span class="qty_delivered">19</span></span>
+<div class="qty_error"></div>
